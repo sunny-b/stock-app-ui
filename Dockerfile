5eca25d54e78f341f95cfcecc1090e8ddc50ceec
Sunny <sjbeatteay@gmail.com>
2021-02-08 20:58:26 -0500

    enable static file serving

--- Dockerfile
+++ Dockerfile
@@ -8,5 +8,5 @@ RUN npm install
 
 COPY . .
 
-CMD [ "node", "app.js" ]
+CMD [ "node", "./bin/www" ]
 
--- app.js
+++ app.js
@@ -54,7 +54,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public'), { dotfiles: 'allow' }));
 app.use(session({
     secret: process.env.SESSION_SECRET,
     resave: true,
